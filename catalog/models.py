cdb741de7a5afe7a32511c349e95a9c020e5b235
Yana <yana.sh93@gmail.com>
2023-01-28 23:22:39 +0200

    Fix build

--- catalog/models.py
+++ catalog/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Retailer(models.Model):
     name = models.CharField(max_length=200)
     city = models.OneToOneField(City, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -15,9 +21,15 @@ class Product(models.Model):
     retailer = models.ForeignKey(Retailer, null=True, on_delete=models.SET_NULL)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     city = models.ForeignKey(City, null=True, on_delete=models.SET_NULL)
     products = models.ManyToManyField(Product, verbose_name="list of products")
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
