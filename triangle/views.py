74951b809889fd10a42d85b2710027b4f5186965
Yana <yana.sh93@gmail.com>
2023-02-02 19:06:00 +0200

    Fix 2

--- triangle/views.py
+++ triangle/views.py
@@ -1,21 +1,15 @@
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from math import sqrt
+from django.shortcuts import render
 
 from .forms import TriangleForm
 
 
 def calculate_triangle(request):
     form = TriangleForm(request.GET)
-    params = {'form': form}
-    if request.GET.get('hypotenuse'):
-        params['result'] = round(float(request.GET.get('hypotenuse')), 3)
-    else:
-        if not form.is_valid():
-            params['error'] = 'Triangle\'s legs should be > 0'
-        else:
-            form.clean()
-            leg_1 = form.cleaned_data.get("leg_1")
-            leg_2 = form.cleaned_data.get("leg_2")
-            hypotenuse = (leg_1**2 + leg_2**2)**0.5
-            return redirect(reverse("triangle:triangle") + "?hypotenuse=" + str(hypotenuse))
-    return render(request, 'triangle/triangle.html', params)
+    params = {"form": form}
+    if form.is_valid():
+        form.clean()
+        leg_1 = form.cleaned_data.get("leg_1")
+        leg_2 = form.cleaned_data.get("leg_2")
+        params["result"] = round(sqrt(leg_1**2 + leg_2**2), 2)
+    return render(request, "triangle/triangle.html", params)
